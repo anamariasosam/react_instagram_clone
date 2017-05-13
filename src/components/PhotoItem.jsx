f4208e5efb589e991409d53c344225540440a1e4
Ana Maria Sosa <anamariasosam@gmail.com>
2017-05-13 11:44:04 -0500

    fix files indentation

--- src/components/PhotoItem.jsx
+++ src/components/PhotoItem.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
 
 const PhotoItem = ({ photo }) => {
-  const imageUrl = photo.images.standard_resolution.url;
-  const title = photo.caption.text
-  const link = photo.link
+  const imageUrl = photo.images.standard_resolution.url,
+  title = photo.caption.text,
+  link = photo.link
 
   return (
-    <li
-      className="list-group-item">
-      <div className="photo-list media">
-        <div className="media-left">
-          <a href={link} target="_blank">
-            <img
-              src={imageUrl}
-              alt={title}
-              className="media-object"
-             />
-          </a>
-        </div>
-
-        <div className="media-body">
-          <div className="media-heading">
-            { title }
-          </div>
-        </div>
-      </div>
+    <li>
+      <a href={link} target="_blank">
+        <img
+          src={imageUrl}
+          alt={title}
+         />
+      </a>
     </li>
   );
 }
